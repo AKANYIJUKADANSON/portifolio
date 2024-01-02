ed2b0f35f00a5b05c4b2a1523023ab53f03fd22d
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2024-01-02 21:31:34 +0300

    Adding the services page

---
+++ services.php
@@ -0,0 +1,87 @@
+<?php
+include('inc/config.php');
+// Get services
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AD|Services</title>
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/myimage.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/myimage.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/myimage.png">
+
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
+  <!-- icons -->
+  <link rel="stylesheet" href="assets/bootstrap/bootstrap-icons/bootstrap-icons.css">
+
+  <!-- custom css Link -->
+  <link rel="stylesheet" href="assets/style.css">
+  <link rel="stylesheet" href="assets/style.scss">
+</head>
+
+<body>
+  <!-- ======= Nav Bar ======= -->
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid" style="border: 2px solid red;">
+      <!-- <a class="navbar-brand d-flex align-items-center" href="#"> -->
+      <!-- <img src="img/logo.png" alt="Logo" width="60" height="60" class="d-inline-block align-text-top"> -->
+      <span><b class="fs-1 text-white navbar-brand">AKANYIJUKA DANSON</b></span>
+      <!-- </a> -->
+
+      <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="bi bi-list fs-2 text-white"></i>
+      </button>
+      <div class="collapse  navbar-collapse justify-content-end" id="navbarTogglerDemo01">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link " href="index.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link " href="about.php">About</a></li>
+          <li class="nav-item"><a id="active" class="nav-link " href="services.php">Services</a></li>
+          <li class="nav-item"><a class="nav-link " href="portfolio.php">Portfolio</a></li>
+          <li class="nav-item"><a class="nav-link " href="contact.php">Contact</a></li>
+
+
+          <li class="nav-item">
+            <span>
+              <div class="nav-link" id="google_translate_element"></div>
+
+              <script type="text/javascript">
+                function googleTranslateElementInit() {
+                  new google.translate.TranslateElement({
+                    pageLanguage: 'en'
+                  }, 'google_translate_element');
+                }
+              </script>
+              <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+            </span>
+
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+
+      <!-- JS -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> -->
+
+    <!-- bootstrap js -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.js"></script>
+
+</body>
+
+</html>
