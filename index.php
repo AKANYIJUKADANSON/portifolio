3f183ac559465e28989eca99064bb78c54a49f46
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2024-01-02 21:23:14 +0300

    Init

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AD|Home</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/myimage.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/myimage.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/myimage.png">
+
+
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
+    <!-- icons -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap-icons/bootstrap-icons.css">
+
+    <!-- Link -->
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/style.scss">
+</head>
+
+<body>
+    <?php include('inc/navbar.php'); ?>
+
+
+    <div class="d-flex text-center align-items-center justify-content-center" style="height: 93vh" id="hero-home-page">
+        <div class="hero-container mx-2">
+            <h1 id="nav-brandi">AKANYIJUKA DANSON</h1>
+            <h2>Software developer from Kampala, Uganda</h2>
+            <div class="">
+                <a href="contact.php" class="btn btn-primary hireme-btn fs-4 fw-bold">Hire Me</a>
+                <a href="" class="btn watch-demo"><i class="bi bi-play-circle-fill fs-1" id=""></i><span class="fs-3 ms-2" id="">Demo</span></a>
+            </div>
+
+            <div class="social-media">
+                <a href="https://twitter.com/home" class="mx-2 text-decoration-none">
+                    <i class="bi bi-twitter text-info fs-1"></i>
+                </a>
+                <a href="https://www.facebook.com/danson.akanyijuka.54" class="mx-2 text-decoration-none">
+                    <i class="bi bi-facebook text-primary fs-1"></i>
+                </a>
+                <a href="https://www.instagram.com/dansonakanyijuka/" class="mx-2 text-decoration-none">
+                    <i class="bi bi-instagram text-danger fs-1"></i>
+                </a>
+                <a href="https://github.com/AKANYIJUKADANSON" class="mx-2 text-decoration-none">
+                    <i class="bi bi-github text-white fs-1"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/akanyijuka-danson-a09579172/" class="mx-2 text-decoration-none">
+                    <i class="bi bi-linkedin text-primary fs-1"></i>
+                </a>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+    <!-- bootstrap js -->
+    <!-- <script src="assets/bootstrap/js/bootstrap.min.js"></script> -->
+    <!-- <script src="assets/bootstrap/js/bootstrap.js"></script> -->
+
+</body>
+
+</html>
